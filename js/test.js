17222e6fa7c38664e15009e04245214e85cabe7d
Azarka1 <Azarka1@yandex.ru>
2021-08-04 01:20:54 +0300

    "Подгонка" по пикселям. Исправление ошибок

--- js/test.js
+++ js/test.js
@@ -1,18 +1,39 @@
-let offset = 0;
-const sliderLine = document.querySelector('.reviews-info');
+const images = document.querySelectorAll('.reviews-main .reviews-info .text');
+const reviewsLine = document.querySelector('.reviews-main .reviews-info');
+let count = 0;
+let width;
 
-document.querySelector('.reviews-next').addEventListener('click', function(){
-    offset = offset + 731;
-    if (offset > 731) {
-        offset = 0;
+function init() {
+    console.log('resize');
+    width = document.querySelector('.reviews-main').offsetWidth;
+    reviewsLine.style.width = width * images.length + 'px';
+    images.forEach(item => {
+        item.style.width = width + 'px';
+        item.style.height = 'auto';
+    });
+    rollReviews();
+}
+
+init();
+window.addEventListener('resize', init);
+
+document.querySelector('.reviews-next').addEventListener('click', function () {
+    count++;
+    if (count >= images.length) {
+        count = 0;
     }
-    sliderLine.style.left = -offset + 'px';
+    rollReviews();
 });
 
 document.querySelector('.reviews-prev').addEventListener('click', function () {
-    offset = offset - 731;
-    if (offset < 0) {
-        offset = 731;
+    count--;
+    if (count < 0) {
+        count = images.length - 1;
     }
-    sliderLine.style.left = -offset + 'px';
+    rollReviews();
 });
+
+function rollReviews() {
+    reviewsLine.style.transform = 'translate(-' + count * width + 'px)';
+
+}
