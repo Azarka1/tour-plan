9919e4efcbdb0d5c73c01b1eff66c81457fc8449
Azarka1 <Azarka1@yandex.ru>
2021-08-15 11:35:15 +0300

    media для footer

--- js/main.js
+++ js/main.js
@@ -18,4 +18,10 @@ const reviews = new Swiper('.reviews-main', {
   },
   });
 
-
+var menuButton = document.querySelector(".header-top-menu-burger-visible");
+menuButton.addEventListener("click", function() {
+console.log("Клик по кнопке");
+document
+.querySelector(".header-navigation-visible")
+.classList.toggle(".header-navigator-conteiner");
+});
