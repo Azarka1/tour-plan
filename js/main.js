d5577a3b17f79425a03ff9469a4d09bd1b3d9219
Azarka1 <Azarka1@yandex.ru>
2021-08-20 13:49:20 +0300

    Modal window

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 
+$(document).ready(function() {
 document.addEventListener("DOMContentLoaded", function(event) {
     const swiper = new Swiper('.swiper-container', {
   // Optional parameters
@@ -28,3 +29,32 @@ document
 .classList.toggle("header-navigator-conteiner--visible");
 });
   });
+
+var menuButton = document.querySelector(".menu_botton");
+menuBotton.addEventListener("click", function(){
+console.log("Клик по крестику");
+document
+.querySelector(".navbar_botton")
+.classList.toggle("navbar_botton--visible");
+});
+
+
+var modalButton = $("[data-toggle=modal]");
+var closeModalButton = $(".modal__close");
+modalButton.on("click", openModal);
+closeModalButton.on("click", closeModal);
+
+function openModal() {
+var modalOverlay = $(".modal__overlay");
+var modalDialog = $(".modal__dialog");
+modalOverlay.addClass("modal__overlay--visible");
+modalDialog.addClass("modal__dialog--visible");
+}
+function closeModal(event) {
+  event.preventDefault();
+var modalOverlay = $(".modal__overlay");
+var modalDialog = $(".modal__dialog");
+modalOverlay.removeClass("modal__overlay--visible");
+modalDialog.removeClass("modal__dialog--visible");
+}
+});
